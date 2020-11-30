251e5e65b4c452b1a6d6cca73023918053b2d4f3
WalkingDuck <1164199234@qq.com>
2020-11-30 15:06:11 +0800

    编写常量类

---
+++ CrowdFunding-common-util/src/main/java/com/wtw/crowd/constraint/CrowdConstraint.java
@@ -0,0 +1,13 @@
+package com.wtw.crowd.constraint;
+
+public class CrowdConstraint {
+	public static final String ATTR_NAME_EXCEPTION = "exception";
+	
+	public static final String MESSAGE_LOGIN_FAILED = "账号或密码错误！请重试！";
+	
+	public static final String MESSAGE_LOGIN_ACCT_ALREADY_IN_USE = "账号已经被使用！";
+	
+	public static final String MESSAGE_ACCESS_FORBIDDEN = "请登录！";
+	
+	
+}

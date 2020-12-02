e5e949855020a87a1697df1550495ec2cf2a9936
WalkingDuck <1164199234@qq.com>
2020-12-02 15:19:41 +0800

    添加了对字符串进行加密处理的功能

--- CrowdFunding-common-util/src/main/java/com/wtw/crowd/util/CrowdUtil.java
+++ CrowdFunding-common-util/src/main/java/com/wtw/crowd/util/CrowdUtil.java
@@ -1,6 +1,12 @@
 package com.wtw.crowd.util;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.servlet.http.HttpServletRequest;
+
+import com.wtw.crowd.constraint.CrowdConstraint;
 
 public class CrowdUtil {
 	
@@ -22,5 +28,48 @@ public class CrowdUtil {
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * 对字符串进行加密处理
+	 * @param source
+	 * @return
+	 */
+	public static String md5(String source) {
+		
+		if(source == null || source.isEmpty()) {
+			
+			//字符串不合法
+			throw new RuntimeException(CrowdConstraint.MESSAGE_STRING_INVALIDATE);
+		}
+		
+		String algorithm = "md5";
+		
+		try {
+			
+			MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+			
+			// 获取source对应的字符数组
+			byte[] input = source.getBytes();
+			
+			// 进行加密
+			byte[] output = messageDigest.digest(input);
+			
+			// 创建biginteger
+			int signum = 1;
+			BigInteger bigInteger = new BigInteger(signum, output);
+			
+			// 将biginteger转换为16进制字符串
+			int radix = 16;
+			String encoded = bigInteger.toString(radix);
+			
+			return encoded;
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }

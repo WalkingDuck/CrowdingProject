ba8e730c81820b84dcc2991820834e1a47462fca
WalkingDuck <1164199234@qq.com>
2020-12-29 00:00:38 +0800

    完成前端分页展示

---
+++ CrowdFunding-admin-webui/src/main/webapp/crowd/my-role.js
@@ -0,0 +1,104 @@
+// 展示分页效果
+function generatePage() {
+	
+	// 获取分页数据
+	var pageInfo = getPageInfoRemote();
+	
+	// 填充标签
+	fillTableBody(pageInfo);
+	
+}
+
+// 获取pageinfo
+function getPageInfoRemote() {
+	$.ajax({
+		"url": "role/get/page/info.json",
+		"type": "post",
+		"data": {
+			"pageSize": window.pageSize,
+			"pageNum": window.pageNum,
+			"keyword": window.keyword
+		},
+		"dataType": "json",
+		"success": function(response) {
+			var pageInfo = response.data;
+			
+			fillTableBody(pageInfo);
+		},
+		"error": function(response) {
+			layer.msg("服务器出错！");
+		}
+	});
+}
+
+// 填充tbody标签
+function fillTableBody(pageInfo) {
+	
+	$("#rolePageBody").empty();
+	
+	if(pageInfo == null || pageInfo == undefined || pageInfo.list == null || pageInfo.list.length == 0) {
+		$("#rolePageBody").append("<tr><td colspan='4'>抱歉！没有查找到相关信息</td></tr>");
+		
+		return ;
+	}
+	
+	var list = pageInfo.list;
+	
+	for(var i = 0; i < list.length; i++) {
+		
+		var role = list[i];
+		
+		var roleId = role.id;
+		
+		var roleName = role.name;
+		
+		var numberTd = "<td>" + (i + 1) + "</td>";
+		
+		var checkBoxTd = "<td><input type='checkbox'></td>";
+		
+		var roleNameTd = "<td>" + roleName + "</td>";
+		
+		var checkBtn = "<button type='button' class='btn btn-success btn-xs'><i class=' glyphicon glyphicon-check'></i></button>"
+		
+		var pencilBtn = "<button type='button' class='btn btn-primary btn-xs'><i class=' glyphicon glyphicon-pencil'></i></button>";
+		
+		var removeBtn = "<button type='button' class='btn btn-danger btn-xs'><i class=' glyphicon glyphicon-remove'></i></button>";
+			
+		var buttonTd = "<td>" + checkBtn + " " + pencilBtn + " " + removeBtn + "</td>";
+		
+		var tr = "<tr>" + numberTd + " " + checkBoxTd + " " + roleNameTd + " " + buttonTd + "</tr>";
+		
+		$("#rolePageBody").append(tr);
+	}
+	
+	gernerateNavigator(pageInfo);
+}
+
+// 执行翻页
+function gernerateNavigator(pageInfo) {
+	
+	// 获取总记录数
+	var totalRecord = pageInfo.total;
+	
+	var prop = {
+			"num_edge_entries": 3,	// 边缘页数
+			"num_display_entries": 5,	// 主体页数
+			"callback": paginationCallBack,	// 回调函数
+			"items_per_page": pageInfo.pageSize,	// 每页显示多少条
+			"current_page": pageInfo.pageNum - 1,	// 当前页数(从0开始)
+			"prev_text": "上一页",		// 上一页按钮显示的文本
+			"next_text": "上一页"		// 下一页按钮显示的文本
+	};
+	
+	$("#pagination").pagination(totalRecord, prop);
+	
+}
+
+// 回调函数
+function paginationCallBack(pageIndex, jQuery) {
+	window.pageNum = pageIndex + 1;
+	
+	generatePage();
+	
+	return false;
+}

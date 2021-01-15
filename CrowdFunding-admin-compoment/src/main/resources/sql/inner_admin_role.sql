c48bf45e2231d37f8c66c4f4787a6468da744c56
WalkingDuck <1164199234@qq.com>
2021-01-16 00:49:01 +0800

    权限分配建表语句

---
+++ CrowdFunding-admin-compoment/src/main/resources/sql/inner_admin_role.sql
@@ -0,0 +1,31 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50562
+ Source Host           : localhost:3306
+ Source Schema         : project_crowd
+
+ Target Server Type    : MySQL
+ Target Server Version : 50562
+ File Encoding         : 65001
+
+ Date: 13/01/2021 22:31:32
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for inner_admin_role
+-- ----------------------------
+DROP TABLE IF EXISTS `inner_admin_role`;
+CREATE TABLE `inner_admin_role`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NULL DEFAULT NULL,
+  `admin_id` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
